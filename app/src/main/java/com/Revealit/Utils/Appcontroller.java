6096b3d3346428ca2590d6fd6d1c08e91c187133
Ketan Patel <ketan.taasha@gmail.com>
2021-05-25 09:39:15 +0530

    Implement Testfairy SDK.

--- app/src/main/java/com/Revealit/Utils/Appcontroller.java
+++ app/src/main/java/com/Revealit/Utils/Appcontroller.java
@@ -1,20 +1,49 @@
 package com.Revealit.Utils;
 
 import android.app.Application;
+import android.content.Context;
 
-import com.droidnet.DroidNet;
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 
-public class Appcontroller extends Application {
+import com.Revealit.R;
+import com.facebook.FacebookSdk;
+import com.testfairy.TestFairy;
+
+public class Appcontroller extends MultiDexApplication {
+    public static final String TAG = Appcontroller.class.getSimpleName();
+    private static Appcontroller mInstance;
+
+
     @Override
-    public void onCreate() {
-        super.onCreate();
-        DroidNet.init(this);
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        //MULTIDEX
+        MultiDex.install(this);
+
+        //FACEBOOK SDK
+        //FacebookSdk.sdkInitialize(this);
+
 
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        DroidNet.getInstance().removeAllInternetConnectivityChangeListeners();
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+
+        //TEST FAIRY SDK
+        TestFairy.begin(this, "SDK-5f7VDJMC");
+
+        //SHAKE FEATURE
+        TestFairy.enableFeedbackForm("shake");
+
+
+    }
+
+    public static synchronized Appcontroller getInstance() {
+        return mInstance;
     }
 }
